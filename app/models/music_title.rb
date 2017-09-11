79c790b53dbe8d4e08e67a42ebcb3d567a015851
AyumiTakashina <moira.horizon@gmail.com>
2017-09-11 08:00:17 +0000

    モデルのアソシエーション

--- app/models/music_title.rb
+++ app/models/music_title.rb
@@ -1,2 +1,4 @@
 class MusicTitle < ApplicationRecord
+  belongs_to :albumname
+  belongs_to :overallconfig
 end
--- app/models/over_all_config.rb
+++ app/models/over_all_config.rb
@@ -1,2 +1,5 @@
 class OverAllConfig < ApplicationRecord
+  has_many :artistnames
+  has_many :albumnames
+  has_many :musictitles
 end
