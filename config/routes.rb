29e0d48197628743ecb4939d29987a82550918b8
AyumiTakashina <moira.horizon@gmail.com>
2017-09-11 07:06:05 +0000

    コントローラーとモデルを作成。コメントはなし

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'musictitles/index'
+
+  get 'musictitles/show'
+
+  get 'albumnames/index'
+
+  get 'albumnames/show'
+
+  get 'artistnames/index'
+
+  get 'artistnames/show'
+
+  get 'overallconfigs/index'
+
+  get 'overallconfigs/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
